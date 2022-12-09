830eafd544360119a21bb8e5ea6e8f85912cc5ec
[Andrea] <[andreadefilippi@gmail.com]>
2022-12-09 10:03:36 +0100

    Test2

---
+++ src/Animal.java
@@ -0,0 +1,10 @@
+public class Animal {
+    String animalName;
+    public Animal (String name){
+        this.animalName = name;
+    }
+    public void animalSound(){
+        System.out.println("Default Animal Sound");
+    }
+}
+
---
+++ src/Start.java
@@ -0,0 +1,10 @@
+public class Start {
+    public static void main (String[] arg){
+        Lion lion = new Lion("Lion");
+        Cow cow = new Cow ("Cow");
+        Animal animal = new Animal("Animal");
+        lion.animalSound();
+        cow.animalSound();
+        animal.animalSound();
+    }
+}
