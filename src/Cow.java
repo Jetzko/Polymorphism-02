8598b94de4d8aae20353dcb6a8c1729785bc4ad1
[Andrea] <[andreadefilippi@gmail.com]>
2022-12-09 10:01:55 +0100

    Test2

---
+++ src/Cow.java
@@ -0,0 +1,10 @@
+public class Cow extends Animal{
+    public Cow(String name) {
+        super(name);
+    }
+
+    @Override
+    public void animalSound() {
+        System.out.println("Mou!");
+    }
+}
---
+++ src/Lion.java
@@ -0,0 +1,10 @@
+public class Lion extends Animal {
+    public Lion(String name) {
+        super(name);
+    }
+
+    @Override
+    public void animalSound() {
+        System.out.println("Roar!");
+    }
+}
